fc5f49d869e75cd0c9a24062e3adc24bec00f1f2
anshmahajan26 <anshmahajan678@gmail.com>
2026-03-25 21:08:04 +0530

    done code

---
+++ Day13/freq.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Freq{
+    public static void main (String args[]){
+        int arr[]= {1,1,2,34,53,53,6};
+        HashMap <Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for(int i =0; i<arr.length; i++){
+            if(hm.containsKey(arr[i])){
+                hm.put(arr[i], hm.get(arr[i]+1));  //if arr of i present then arr[i]+1
+
+            }
+            else{
+                hm.put(arr[i],1);
+            }
+        }
+
+        for(Map.Entry entry:hm.entrySet()){
+            System.out.println("Element | Frequency");
+            System.out.println(entry.getKey() + "   "+ entry.getValue());
+        }
+    }
+}
